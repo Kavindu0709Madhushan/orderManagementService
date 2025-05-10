320c63a61b066029ac09ab524808f2f689137880
kavindu0907 <agkavindu2001@gmail.com>
2025-05-10 11:11:00 +0530

    includepatch

--- order-service/server.js
+++ order-service/server.js
@@ -13,7 +13,7 @@ const app = express();
 // }));
 app.use(cors({
     origin: "*", //  Allow all origins (not safe for production)
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
 }));
 
 app.use(express.json());
