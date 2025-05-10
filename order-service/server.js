ecd3a5d446ff38a435733b0f36e9fe5467021fc9
kavindu0907 <agkavindu2001@gmail.com>
2025-05-10 10:11:13 +0530

    newupdateall origin

--- order-service/server.js
+++ order-service/server.js
@@ -6,14 +6,17 @@ const connectDB = require("./config/db");
 const app = express();
 
 // ✅ Correct CORS settings for local React frontend
+// app.use(cors({
+//     origin: ["http://localhost:5173","https://frontend-bite-speed.vercel.app"],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     credentials: true  // true only if sending cookies/auth headers
+// }));
 app.use(cors({
-    origin: ["http://localhost:5173","https://frontend-bite-speed.vercel.app"],
+    origin: "*", //  Allow all origins (not safe for production)
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true  // true only if sending cookies/auth headers
 }));
 
 app.use(express.json());
-
 connectDB();
 
 app.use("/api/orders", require("./routes/orderRoutes"));
